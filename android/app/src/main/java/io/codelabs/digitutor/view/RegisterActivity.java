6e8b474c34a2fc680b49407e68a700be553fda65
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-04-30 20:21:08 +0000

    Added icons

--- android/app/src/main/java/io/codelabs/digitutor/view/RegisterActivity.java
+++ android/app/src/main/java/io/codelabs/digitutor/view/RegisterActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 
 import io.codelabs.digitutor.R;
 import io.codelabs.digitutor.core.base.BaseActivity;
+import io.codelabs.digitutor.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends BaseActivity {
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register);
+
+        binding.loading.setVisibility(View.GONE);
     }
 
     public void createUser(View view) {
--- android/app/src/main/java/io/codelabs/digitutor/view/ResetPassword.java
+++ android/app/src/main/java/io/codelabs/digitutor/view/ResetPassword.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 
 import io.codelabs.digitutor.R;
 import io.codelabs.digitutor.core.base.BaseActivity;
+import io.codelabs.digitutor.databinding.ActivityResetBinding;
 
 public class ResetPassword extends BaseActivity {
+    private ActivityResetBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_reset);
 
+        binding.loading.setVisibility(View.GONE);
 
     }
 
